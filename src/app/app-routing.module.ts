f270c6199decd89ce2fc8d71a7e5c1726d04b031
sree-angular <sreedivyachintala546@gmail.com>
2025-06-14 09:37:38 +0530

    added required components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { AboutComponent } from './about/about.component';
+import { AddRecipeComponent } from './add-recipe/add-recipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' ,redirectTo: '/home', pathMatch: 'full'},
+  {path:'home', component:HomeComponent},
+  {path:'recipes', component:RecipesComponent},
+  {path:'about',component:AboutComponent},
+  {path:'add', component:AddRecipeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
